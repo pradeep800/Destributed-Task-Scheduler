8e8ee655412ddb61c506b1d49a18bf0c3388293a
pradeep <pradeep8b0@gmail.com>
2024-07-19 19:36:45 +0530

    day 5

--- pub-api/migrations/20240716133613_tasks.sql
+++ pub-api/migrations/20240716133613_tasks.sql
@@ -1,10 +1,9 @@
 
-CREATE TYPE status AS ENUM ('COMPLETED', 'PROCESSING', 'FAILED','ADDED');
 
 CREATE TABLE Tasks (
     id SERIAL PRIMARY KEY, 
     schedule_at_in_second INTEGER NOT NULL,
-    status status NOT NULL, 
+    status VARCHAR(256) NOT NULL, 
     output TEXT NOT NULL,  
     retry SMALLINT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL 
--- pub-api/src/routes/tasks.rs
+++ pub-api/src/routes/tasks.rs
@@ -1,4 +1,6 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use chrono::{DateTime, Timelike, Utc};
+use serde_json::json;
 use std::sync::Arc;
 
 use tracing::error;
@@ -14,36 +16,73 @@ impl IntoResponse for AppError {
         (StatusCode::INTERNAL_SERVER_ERROR, format!("{}", self.0)).into_response()
     }
 }
-
-#[derive(sqlx::Type)]
-#[sqlx(type_name = "status")]
-#[sqlx(rename_all = "UPPERCASE")]
-enum Status {
-    Completed,
-    Processing,
-    Failed,
-    Added,
-}
-#[derive(sqlx::FromRow)]
-struct Tasks {
-    id: String,
+#[derive(serde::Deserialize)]
+pub struct Tasks {
     schedule_at_in_second: i32,
-    status: Status,
-    output: String,
     retry: i16,
-    created_at: chrono::DateTime<chrono::Local>,
 }
 pub async fn create_task(
     State(state): State<Arc<AppState>>,
+    Json(task): Json<Tasks>,
 ) -> Result<impl IntoResponse, AppError> {
-    let query = sqlx::query_as!(Tasks, "select * from tasks")
-        .fetch_one(&state.pool)
-        .await
-        .unwrap();
-    Ok((StatusCode::OK, "hello"))
+    println!("hello world");
+    if task.retry > 3 || task.retry < 0 {
+        return Ok((
+            StatusCode::BAD_REQUEST,
+            Json(json!({"error":"tasks retry should be atleast 1 and max 3"})),
+        ));
+    }
+    let today_time_in_second = Utc::now().second() + 30;
+    if task.schedule_at_in_second < today_time_in_second as i32 {
+        return Ok((
+            StatusCode::BAD_REQUEST,
+            Json(
+                json!({"error":"schedule_at_in_second should be alteast greater than now (in server)"}),
+            ),
+        ));
+    }
+    sqlx::query!(
+        "INSERT INTO Tasks (schedule_at_in_second, status, output, retry, created_at) 
+         VALUES ($1,$2,$3,$4,$5)",
+        task.schedule_at_in_second as i32,
+        "ADDED",
+        "",
+        task.retry,
+        Utc::now()
+    )
+    .execute(&state.pool.clone())
+    .await
+    .map_err(|e| AppError(anyhow::Error::new(e)))?;
+    return Ok((StatusCode::OK, Json(json!({"status":"ADDED"}))));
+}
+#[derive(serde::Deserialize)]
+pub struct Id {
+    pub id: i32,
+}
+#[derive(serde::Serialize)]
+pub struct Task {
+    pub id: i32,
+    pub schedule_at_in_second: i32,
+    pub status: String,
+    pub ouput: String,
+    pub retry: i16,
+    pub created_at: DateTime<Utc>,
 }
 pub async fn check_status(
     State(state): State<Arc<AppState>>,
+    Json(body): Json<Id>,
 ) -> Result<impl IntoResponse, AppError> {
-    Ok((StatusCode::OK, "hello"))
+    let current_task = sqlx::query!("Select * from tasks where id=$1", body.id)
+        .fetch_one(&state.pool)
+        .await
+        .map_err(|e| AppError(anyhow::Error::new(e)))?;
+
+    Ok(Json(Task {
+        id: current_task.id,
+        schedule_at_in_second: current_task.schedule_at_in_second,
+        status: current_task.status,
+        ouput: current_task.output,
+        retry: current_task.retry,
+        created_at: current_task.created_at,
+    }))
 }
