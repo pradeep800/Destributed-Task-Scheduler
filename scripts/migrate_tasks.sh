0807dcdbccb2d05391c48ee0759acbf5b0934639
pradeep <pradeep8b0@gmail.com>
2024-08-04 09:42:43 +0530

    day 19

--- scripts/migrate_tasks.sh
+++ scripts/migrate_tasks.sh
@@ -3,9 +3,9 @@ set -x
 set -eo pipefail
 DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-DB_NAME="${POSTGRES_DB:=health_check}"
-DB_PORT="${POSTGRES_PORT:=5433}"
+DB_NAME="${POSTGRES_DB:=tasks}"
+DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 sqlx database create
-sqlx migrate run
+sqlx migrate run --source db/tasks/migrations 
